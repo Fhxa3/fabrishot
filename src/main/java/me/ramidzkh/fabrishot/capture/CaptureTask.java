dc0288cc845a8c479fa8b109af5a7e67016e6432
”didanko228” <artemdidanov072@gmail.com>
2025-12-24 20:51:25 +0300

    port to 26.1-snapshot-1

--- src/main/java/me/ramidzkh/fabrishot/capture/CaptureTask.java
+++ src/main/java/me/ramidzkh/fabrishot/capture/CaptureTask.java
@@ -26,9 +26,9 @@ package me.ramidzkh.fabrishot.capture;
 
 import me.ramidzkh.fabrishot.config.Config;
 import me.ramidzkh.fabrishot.event.FramebufferCaptureCallback;
-import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.Screenshot;
+import net.minecraft.util.Util;
 
 import java.io.IOException;
 import java.nio.file.Path;
